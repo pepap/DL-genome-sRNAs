29b84f4d3eede8da17f0cb21cc3564560f900104
pepap <jpasulka@gmail.com>
2025-06-21 10:34:38 +0200

    Update 01.02.PREP-INPUT--map.bash

--- miRNAs/01.02.PREP-INPUT--map.bash
+++ miRNAs/01.02.PREP-INPUT--map.bash
@@ -1,35 +1,34 @@
 #!/bin/bash
 
+metamodule add fastx-0.0.14
+
+FAMRGZ=(
+ INPUT_READ_FILE1.fa.gz
+ INPUT_READ_FILE2.fq.gz
+ ...
+)
+
+zcat ${FAMRGZ[@]} | fastx_collapser -v -i - -o all-DL-smallRNAseq-merged.re-collapsed.fa
+
+gzip -v all-DL-smallRNAseq-merged.re-collapsed.fa
+
 metamodule load star-2.7.7a
 metamodule add samtools-1.9
 
 exeCMD="STAR"
-REF="/storage/brno12-cerit/home/pepap/brno1/TobiasBer/10.DLgenome-CZ-1.0/Deroceras_laeve_CZ.1.0.fasta"
-IND="/storage/brno12-cerit/home/pepap/brno1/TobiasBer/10.DLgenome-CZ-1.0/STAR--2.7.7a_index"
-STOR="/storage/brno12-cerit/home/pepap/brno1/TobiasBer/10.DLgenome-CZ-1.0/BAM.smallRNAseq/00.stats/04.mireap/01.merge_input_reads"
-
-FASTQS=(
- all-DL
-)
-OUTBAM=(
- all-DL
-)
-LIB_PE=(
- SINGLE
-)
-SUFF="-smallRNAseq-merged.re-collapsed.fa.gz"
-
-i=0
+REF="/PATH/TO/INPUT/GENOMIC/FASTA"
+IND="/PATH/TO/STAR/INDEX"
+STOR="all-DL-smallRNAseq-merged.re-collapsed.fa.gz"
+OUTBAM="all-DL"
 
 ${exeCMD} --runMode alignReads \
           --runThreadN  4 \
           --genomeDir ${IND} \
           --genomeLoad LoadAndRemove \
-          --readFilesIn ${FASTQS[$i]}${SUFF} \
-          --readFilesPrefix ${STOR}/ \
+          --readFilesIn ${ICOLFAGZ} \
           --readFilesCommand zcat \
           --limitBAMsortRAM  20000000000 \
-          --outFileNamePrefix ${OUTBAM[$i]}.se. \
+          --outFileNamePrefix ${OUTBAM}.se. \
           --outReadsUnmapped Fastx \
           --outSAMtype BAM SortedByCoordinate \
           --outFilterMultimapNmax 99999 \
@@ -38,9 +37,4 @@ ${exeCMD} --runMode alignReads \
           --alignSJoverhangMin   999 \
           --alignSJDBoverhangMin 999 
 
-samtools index ${OUTBAM[$i]}.se.Aligned.sortedByCoord.out.bam
-
-metamodule add fastQC-0.11.5
-fastqc ${OUTBAM[$i]}.se.Unmapped.out.mate1
-gzip   ${OUTBAM[$i]}.se.Unmapped.out.mate1
-
+samtools index ${OUTBAM}.se.Aligned.sortedByCoord.out.bam
